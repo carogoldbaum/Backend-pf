b6421310af4f9d275c04d9a0cfae90076f942b32
Android Studio <devandroid@ort>
2022-06-24 08:47:03 -0300

    listo

--- controllers/rubroController.js
+++ controllers/rubroController.js
@@ -4,10 +4,10 @@ import { RubroService } from '../services/rubroService.js';
 const router = Router();
 const rubroService = new RubroService();
 
-router.get('', async (req, res) => {
+router.get('/', async (req, res) => {
   console.log(`This is a get operation`);
   
-  const rubro = await rubroService.getRubro(req.query.Rubro);
+  const rubro = await rubroService.getRubro();
 
   return res.status(200).json(rubro);
 });
--- services/rubroService.js
+++ services/rubroService.js
@@ -2,21 +2,14 @@ import sql from 'mssql'
 import config from '../db.js'
 import 'dotenv/config'
 
-const rubroTabla = process.env.DB_TABLA_RUBROS;
+const rubroTabla = process.env.DB_TABLA_RUBRO;
 
 export class RubroService {
-        getRubro = async (rubro) => {
+        getRubro = async () => {
             console.log('This is a function on the service');
-            let query=`select * from ${rubroTabla}`;
-            /*
-            if (rubro){
-                query+=" where rubro=@Rubro";
-            }*/ 
             const pool = await sql.connect(config);
             const response = await pool.request()
-            //.input('Rubro', sql.VarChar, rubro)
-            .query(query);
-        
+                .query(`select * from ${rubroTabla}`);
             console.log(response)
             return response.recordset;
         }    
