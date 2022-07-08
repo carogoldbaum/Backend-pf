a4c2d077772cdccf61e7a4e7656bf5bd9f6618b8
Android Studio <devandroid@ort>
2022-07-08 12:03:47 -0300

    Validar mail y restablecer epmezado

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -41,20 +41,42 @@ router.post('', async (req, res) => {
   return res.status(201).json(usuario);
 });
 
-router.post('', async (req, res) => {
-  console.log(`This is a post operation`);
+router.post('/registrarse', async (req, res) => {
+  console.log(`This is a CArolina post operation`);
 
   const iniciarCuenta = await usuarioService.registrarseInicial(req.body);
-
+  
   return res.status(201).json(iniciarCuenta);
 });
 
 router.post('/login', async (req, res) => {
-  console.log(`This is a get operation`);
+  console.log(`This is a post operation`);
   
   const LogIn = await usuarioService.IniciarSesion(req.body);
 
   return res.status(200).json(LogIn);
+});
+
+router.post('/restablecer', async (req, res) => {
+  console.log(`This is a post operation`);
+  
+  const InfoRestablecer = await usuarioService.Restablecer(req.body);
+
+  return res.status(200).json(InfoRestablecer);
+});
+
+router.post('/MailDiferente', async (req, res) => {
+  console.log(`This is a post operation`);
+  
+  const Mail = await usuarioService.MailDiferente(req.body.Mail);
+
+  if (Mail !=null){
+    return res.status(200).json(Mail);
+  }else{
+    return res.status(404).json("El Mail ya esta en uso");
+  }
+
+  
 });
 
 export default router;
--- services/usuarioService.js
+++ services/usuarioService.js
@@ -19,7 +19,7 @@ export class UsuarioService {
             .input('Password',sql.VarChar, usuario?.Password ?? '')
             .input('FechaNacimiento',sql.Date, usuario?.FechaNacimiento ?? '2022-12-30')
             .input('Opinion',sql.VarChar, usuario?.Opinion ?? '')
-            .query(`UPDATE ${usuarioTabla} SET DNI=@DNI, foto=@foto, NombreApellido=@NombreApellido, Celular=@Celular, Mail=@Mail, Password=@Password, FechaNacimiento=@FechaNacimiento, Opinion=@Opinion`);
+            .query(`UPDATE ${usuarioTabla} SET DNI=@DNI, foto=@foto, NombreApellido=@NombreApellido, Celular=@Celular, FechaNacimiento=@FechaNacimiento, Opinion=@Opinion WHERE Mail=@Mail AND Password=@Password`);
         console.log(response)
 
         return response.recordset;
@@ -27,15 +27,26 @@ export class UsuarioService {
 
     registrarseInicial = async (iniciarCuenta) => {
         console.log('This is a function on the service');
+        console.log(iniciarCuenta);
 
-        const pool = await sql.connect(config);
-        const response = await pool.request()
-            .input('Password',sql.VarChar, iniciarCuenta?.Password ?? '')
-            .input('Mail',sql.VarChar, iniciarCuenta?.Mail ?? '')
-            .query(`INSERT INTO ${usuarioTabla}(Mail, Password) VALUES (@Mail, @Password)`);
-        console.log(response)
-
-        return response.recordset;
+        /*
+        Verifico si existe x email!
+        Si SI existe... return 
+        Si no existe.. hago el insert
+        */
+        let existe = await this.MailDiferente(iniciarCuenta.Mail);
+        console.log(existe)
+        if  (existe){
+            const pool = await sql.connect(config);
+            const response = await pool.request()
+                .input('Password',sql.VarChar, iniciarCuenta?.Password ?? '')
+                .input('Mail',sql.VarChar, iniciarCuenta?.Mail ?? '')
+                .query(`INSERT INTO ${usuarioTabla}(Mail, Password) VALUES (@Mail, @Password)`);
+                return response.recordset;
+        }else{
+            return null;
+        }
+        
     }
 
     IniciarSesion = async (LogIn) => {
@@ -51,4 +62,32 @@ export class UsuarioService {
             return response.recordset;
         }    
 
+        Restablecer = async (InfoRestablecer) => {
+            console.log('This is a function on the service');
+
+            const pool = await sql.connect(config);
+            const response = await pool.request()
+            .input('Password',sql.VarChar, InfoRestablecer?.Password ?? '')
+            .input('Mail',sql.VarChar, InfoRestablecer?.Mail ?? '')
+            .query(`UPDATE ${usuarioTabla} SET Password=@Password WHERE Mail=@Mail`);
+            console.log(response)
+
+            return response.recordset;
+        }    
+
+        MailDiferente = async (MailIngresado) => {
+            let respuesta = false;
+            console.log('This is a function on the service');
+            const pool = await sql.connect(config);
+            const response = await pool.request()
+            .input('Mail',sql.VarChar, MailIngresado?.Mail ?? '')
+            .query(`select * from ${usuarioTabla} WHERE Mail=@Mail`);
+
+            if(response.recordset.length != 0){
+                respuesta = true;
+            }
+
+            return respuesta;
+        
+        }    
 }
