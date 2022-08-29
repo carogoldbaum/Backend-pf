992aadc983b4f472cc7928213edd3d2d5baddd0a
unknown <cachi200@gmail.com>
2022-08-29 17:11:00 -0300

    en postman funcionan

--- controllers/TokenController.js
+++ controllers/TokenController.js
@@ -4,7 +4,7 @@ import {TokenService} from '../services/TokenServices.js';
 const router = Router();
 const tokenService = new TokenService();
 
-router.get('', async (req, res) => {
+router.get('/token', async (req, res) => {
   console.log(`This is a get operation`);
   const token = await tokenService.getToken();
   console.log(token);
