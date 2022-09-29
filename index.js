cdf4de6b27fee43df5ef8e7e03c3c097241adb3e
carogoldbaum <85694442+carogoldbaum@users.noreply.github.com>
2022-09-28 23:15:34 -0300

    .

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ const specs = swaggerJsdoc (swaggerSpec);
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 passport.use(jwtStrategy);
 app.use(passport.initialize());
 
