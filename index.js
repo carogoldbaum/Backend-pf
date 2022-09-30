0d452aaf832c0506914b99d75752e48bbb894dd5
carogoldbaum <85694442+carogoldbaum@users.noreply.github.com>
2022-09-30 09:38:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,10 @@ import TokenRouter from './controllers/TokenController.js';
 import usuarioRouter from "./controllers/usuarioController.js";
 import rubroRouter from "./controllers/rubroController.js";
 import path from "path";
-
 import mysql from 'mysql';
-
 //swagger
 import swaggerUI from "swagger-ui-express";
 import swaggerJsdoc from "swagger-jsdoc";
-
-
 const swaggerSpec = {
   definition: {
     openapi: "3.0.0",
@@ -29,25 +25,19 @@ const swaggerSpec = {
   },
   apis: [`${path.join("localhost:5000", "../controllers/*.js")}`]
 }
-
 const app = express();
 const PORT = 3000;
 const specs = swaggerJsdoc (swaggerSpec);
-
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 passport.use(jwtStrategy);
 app.use(passport.initialize());
 
 app.use("/usuario", usuarioRouter);
-app.use("/rubros", rubroRouter);
+app.use("/rubro", rubroRouter);
 app.use("/auth", TokenRouter);
 //app.use("/api-doc", swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)));
 app.use("/api-doc", swaggerUI.serve, swaggerUI.setup(specs));
-
-
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Live at ${process.env.PORT || PORT}`);
 })
-
