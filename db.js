56a5c0109e32eb54957b02dbab216bbb7f249381
carogoldbaum <85694442+carogoldbaum@users.noreply.github.com>
2022-09-30 09:36:59 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,20 +1,18 @@
 import 'dotenv/config'
 import mysql from 'mysql2/promise'
 
+var conexion = await mysql.createConnection({
+      host: process.env.HOST,
+      database: process.env.DATABASE,
+      user: process.env.USER,
+      password: process.env.PASSWORD,
 
-export default async function connectionRequest() {
-    var conexion = await mysql.createConnection({
-        host: process.env.HOST,
-        database: process.env.DATABASE,
-        user: process.env.USER,
-        password: process.env.PASSWORD,
-    });
+});
 
 
-    conexion.connect(function (err) {
-        if (err) throw err;
-        console.log("Database Connected!");
-    });
-    return conexion;
-}
+conexion.connect(function(err) {
+  if (err) throw err;
+  console.log("Database Connected!");
+});
 
+export default conexion;
