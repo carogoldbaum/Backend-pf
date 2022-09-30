2b47c5487e9b03c7b2cd50f0625a4ac7bb706c4f
carogoldbaum <85694442+carogoldbaum@users.noreply.github.com>
2022-09-30 07:49:24 -0300

    ,,

--- services/rubroService.js
+++ services/rubroService.js
@@ -1,14 +1,15 @@
 import sql from 'mssql'
-import conexion from '../db.js'
 import 'dotenv/config'
-
+import connectionRequest from '../db.js'
 const rubroTabla = process.env.DB_TABLA_RUBRO;
 
 export class RubroService {
         getRubro = async () => { //funciona
             console.log('Get All rubros in rubros Service');
             let query=`SELECT * from rubros`
+            const conexion = connectionRequest();
             const [result,fields] = await conexion.execute(query);
+            conexion.destroy();
             console.log(result);
             return result;
         }    
