ae3475e46fa0471aeb3aa31400a08c51df2c76c0
carogoldbaum <85694442+carogoldbaum@users.noreply.github.com>
2022-09-30 07:49:52 -0300

    ...

--- services/usuarioService.js
+++ services/usuarioService.js
@@ -1,7 +1,5 @@
-import conexion from '../db.js'
 import 'dotenv/config'
-import { response } from 'express';
-
+import connectionRequest from '../db.js'
 const usuarioTabla = process.env.DB_TABLA_USUARIO;
 
 export class UsuarioService {
@@ -9,41 +7,41 @@ export class UsuarioService {
     DatosPersonales = async (usuario) => {  //funciona caro
       
         let Id = await this.UltimoId();
-        console.log(usuario)
-        console.log("drgtsdiuswihkishuhseisehdiuhyfeis",usuario)
-
         let query=`UPDATE usuario set DNI = ?, NombreApellido= ?, Celular= ?, FechaNacimiento= ? WHERE IdUsuario= ?`
- 
         let values=[usuario.DNI, usuario.NombreApellido, usuario.Celular, usuario.FechaNacimiento, Id]
-
+        const conexion = await connectionRequest();
         const [result,fields] = await conexion.execute(query, values);
+        conexion.destroy();
         return result;
     }
 
     registrarse = async (iniciarCuenta) => { //funciona caro
    
         const mailExiste = await this.MailDiferente(iniciarCuenta.email);
-        
+        const conexion = await connectionRequest();
+
         if  (mailExiste == false){
                 let query=`INSERT INTO usuario (email, password) VALUES (?, ?)`
  
                 let values=[iniciarCuenta.email, iniciarCuenta.password]
-        
                 const [result,fields] = await conexion.execute(query, values);
+                conexion.destroy();
             }
             else{
+                conexion.destroy();
                 return "Mail ya existe";
         }
         }
 
         IniciarSesion = async (LogIn) => { //funciona caro
             let validar = false
-       
+            const conexion = await connectionRequest();
             let query=`SELECT * from usuario where password=? AND email=?`
  
             let values=[LogIn.password, LogIn.email]
 
             const [result,fields] = await conexion.execute(query, values);
+            conexion.destroy();
 
             if(result.length != 0){
                 validar = true;
@@ -56,9 +54,10 @@ export class UsuarioService {
             let query=`UPDATE usuario SET Password = ? WHERE email= ?`
  
             let values=[InfoRestablecer.password, InfoRestablecer.email]
-    
+            const conexion = await connectionRequest();
             const [result,fields] = await conexion.execute(query, values);
-          
+            conexion.destroy();
+
             return result;
         }    
 
@@ -68,10 +67,11 @@ export class UsuarioService {
             
             let query=`SELECT * from usuario WHERE email= ?`
             let values=[MailIngresado]
-
+            const conexion = await connectionRequest();
             const [result,fields] = await conexion.execute(query, values);
-           
-           if(result.length != 0){
+            conexion.destroy();
+
+            if(result.length != 0){
                 MailExiste = true;
             }
 
@@ -81,8 +81,9 @@ export class UsuarioService {
         UltimoId = async () => { //funciona caro
         
             let query=`SELECT MAX(IdUsuario) as ultimoId from usuario`
-          
+            const conexion = await connectionRequest();
             const [result,fields] = await conexion.execute(query);
+            conexion.destroy();
 
             return result[0].ultimoId;
         }    
