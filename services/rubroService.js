df83deae2323eab7161e4720552e9c439fce40b3
carogoldbaum <85694442+carogoldbaum@users.noreply.github.com>
2022-09-30 09:09:42 -0300

    Update rubroService.js

--- services/rubroService.js
+++ services/rubroService.js
@@ -4,12 +4,10 @@ import connectionRequest from '../db.js'
 const rubroTabla = process.env.DB_TABLA_RUBRO;
 
 export class RubroService {
-        getRubro = async () => { //funciona
+     getRubro = async () => { //funciona
             console.log('Get All rubros in rubros Service');
             let query=`SELECT * from rubros`
-            const conexion = connectionRequest();
             const [result,fields] = await conexion.execute(query);
-            conexion.destroy();
             console.log(result);
             return result;
         }    
